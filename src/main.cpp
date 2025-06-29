35fab622668c1026e8cae4c106a42cc45ef3d356
Akshay Pal <akshay.anu@gmail.com>
2025-06-29 19:53:42 +0100

    Added lexer and lexer tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "bloch/version/version.hpp"
+#include "bloch/lexer/lexer.hpp"
 
 int main() {
     std::cout << "Bloch Language Compiler v" << bloch::version << std::endl;
+
+    bloch::Lexer lexer();
+    
     return 0;
 }
