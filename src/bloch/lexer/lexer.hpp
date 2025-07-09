1277719e7a4683467c733c68de324fff8d3db05f
Akshay Pal <akshay.anu@gmail.com>
2025-07-09 18:58:58 +0100

    #34: optimise lexer and parser interfaces

--- src/bloch/lexer/lexer.hpp
+++ src/bloch/lexer/lexer.hpp
@@ -8,30 +8,30 @@
 namespace bloch {
     class Lexer {
        public:
-        explicit Lexer(const std::string& source);
-        std::vector<Token> tokenize();
+        explicit Lexer(const std::string_view source) noexcept;
+        [[nodiscard]] std::vector<Token> tokenize();
 
        private:
-        std::string m_source;
+        std::string_view m_source;
         size_t m_position;
         int m_line;
         int m_column;
 
-        char peek() const;
-        char peekNext() const;
-        char advance();
+        [[nodiscard]] char peek() const noexcept;
+        [[nodiscard]] char peekNext() const noexcept;
+        [[nodiscard]] char advance() noexcept;
 
-        bool match(char expected);
+        [[nodiscard]] bool match(char expected) noexcept;
 
         void skipWhitespace();
         void skipComment();
-        void reportError(const std::string& msg);
+        [[noreturn]] void reportError(const std::string& msg);
 
-        Token makeToken(TokenType type, const std::string& value);
-        Token scanToken();
-        Token scanNumber();
-        Token scanIdentifierOrKeyword();
-        Token scanString();
-        Token scanChar();
+        [[nodiscard]] Token makeToken(TokenType type, const std::string& value);
+        [[nodiscard]] Token scanToken();
+        [[nodiscard]] Token scanNumber();
+        [[nodiscard]] Token scanIdentifierOrKeyword();
+        [[nodiscard]] Token scanString();
+        [[nodiscard]] Token scanChar();
     };
 }
