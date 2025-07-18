f47cb8cd4c49b50e3f582263da868009fc2c8c96
Akshay Pal <akshay.anu@gmail.com>
2025-07-18 23:01:43 +0100

    #68: added first pass of quantum simulator

---
+++ src/bloch/runtime/qasm_simulator.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <array>
+#include <complex>
+#include <string>
+#include <vector>
+
+namespace bloch {
+
+    class QasmSimulator {
+       public:
+        int allocateQubit();
+        void h(int q);
+        void x(int q);
+        void y(int q);
+        void z(int q);
+        void rx(int q, double theta);
+        void ry(int q, double theta);
+        void rz(int q, double theta);
+        void cx(int control, int target);
+        int measure(int q);
+        std::string getQasm() const;
+
+       private:
+        int m_qubits = 0;
+        std::vector<std::complex<double>> m_state{1, 0};
+        std::string m_ops;
+
+        void applySingleQubitGate(int q, const std::array<std::complex<double>, 4>& m);
+    };
+
+}
