904cb322ce8aa1d0940fdfb9bed0c29a29af74e3
Akshay Pal <akshay@bloch-labs.com>
2025-09-24 01:30:12 +0100

    ci: add new gh actions for e2e release flow (#140)

--- commitlint.config.js
+++ commitlint.config.js
@@ -15,19 +15,30 @@ const ALLOWED_TYPES = [
 ];
 
 const typePattern = ALLOWED_TYPES.join('|');
+const HEADER_REGEX = new RegExp(`^(?:${typePattern})(?:\\([^)]+\\))?!?: .+ \\(#\\d+\\)$`, 'i');
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  plugins: [
+    {
+      rules: {
+        'header-contains-issue': ({ header }) => {
+          const valid = HEADER_REGEX.test(header || '');
+          return [
+            valid,
+            'commit header must follow "type: summary (#123)" and include an issue number'
+          ];
+        },
+      },
+    },
+  ],
   rules: {
     'type-enum': [2, 'always', ALLOWED_TYPES],
     'subject-case': [0],
     'scope-case': [2, 'always', ['lower-case', 'kebab-case']],
     'header-max-length': [2, 'always', 100],
-    'header-match-pattern': [
-      2,
-      'always',
-      `^(?:${typePattern})(?:\([a-z0-9\-\/]+\))?!?: .+ \(#\\d+\)$`
-    ],
+    'references-empty': [2, 'never'],
+    'header-contains-issue': [2, 'always'],
   },
   ignores: [
     (message) => message.startsWith('Merge'),
