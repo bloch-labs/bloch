fc4075987528f2c0c1e70659cefb77c39f8a8da1
Akshay Pal <akshay@bloch-labs.com>
2025-10-21 23:01:00 +0100

    feat: added feature flag config and class system flag

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,44 +1,24 @@
 const ALLOWED_TYPES = [
+  'build',
+  'chore',
+  'ci',
+  'docs',
   'feat',
   'fix',
-  'bugfix',
-  'hotfix',
-  'chore',
-  'docs',
-  'refactor',
   'perf',
-  'test',
-  'build',
-  'ci',
+  'refactor',
+  'revert',
   'style',
-  'revert'
+  'test',
 ];
-
-const typePattern = ALLOWED_TYPES.join('|');
-const HEADER_REGEX = new RegExp(`^(?:${typePattern})(?:\\([^)]+\\))?!?: .+ \\(#\\d+\\)$`, 'i');
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  plugins: [
-    {
-      rules: {
-        'header-contains-issue': ({ header }) => {
-          const valid = HEADER_REGEX.test(header || '');
-          return [
-            valid,
-            'commit header must follow "type: summary (#123)" and include an issue number'
-          ];
-        },
-      },
-    },
-  ],
   rules: {
     'type-enum': [2, 'always', ALLOWED_TYPES],
     'subject-case': [0],
     'scope-case': [2, 'always', ['lower-case', 'kebab-case']],
     'header-max-length': [2, 'always', 100],
-    'references-empty': [2, 'never'],
-    'header-contains-issue': [2, 'always'],
   },
   ignores: [
     (message) => message.startsWith('Merge'),
